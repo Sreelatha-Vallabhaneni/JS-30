1d4d9b140a085583c28cbf68fbd2d931142d0419
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2019-11-24 01:21:44 +0100

    created setTime function in .js file and added styling

--- CSS-JS-clock/main.js
+++ CSS-JS-clock/main.js
@@ -6,5 +6,14 @@ const seconds = document.querySelector(".seconds");
 const setTime = () => {
     const date = new Date();
     const setSeconds = date.getSeconds();
-    const secondsDegrees = ((setSeconds / 60) )
+    const secondsDegrees = ((setSeconds / 60) * 360) + 90;
+    seconds.style.transform = `rotate(${secondsDegrees}deg)`;
+    const setMins = date.getMinutes();
+    const minsDegrees = ((setMins / 60) * 360) + ((setSeconds / 60) * 6) + 90;
+    mins.style.transform = `rotate(${minsDegrees}deg)`;
+    const setHour = date.getHours();
+    const hourDegrees = ((setHour / 12) * 360) + ((setMins / 60) * 30) + 90;
+    hours.style.transform = `rotate(${hourDegrees}deg)`;
 }
+setInterval(setTime, 1000);
+setTime();
